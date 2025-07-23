2eee8529bcba138194a9faa7cbcee95ceee727af
Supakornn <78318848+Supakornn@users.noreply.github.com>
2025-07-23 23:48:54 +0700

    edit age_restriction

--- class4_romantica.py
+++ class4_romantica.py
@@ -31,7 +31,7 @@ def main():
         {'movie_name': 'Inception', 'ticket_price': 280, 'genre': 'Sci-Fi', 'age_restriction': '13+'},
         {'movie_name': 'It', 'ticket_price': 180, 'genre': 'Horror', 'age_restriction': '18+'},
         {'movie_name': 'The Notebook', 'ticket_price': 250, 'genre': 'Romantic', 'age_restriction': '13+'},
-        {'movie_name': 'Harry Potter and the Sorcerer\'s Stone', 'ticket_price': 260, 'genre': 'Fantasy', 'age_restriction': 'PG'}
+        {'movie_name': 'Harry Potter and the Sorcerer\'s Stone', 'ticket_price': 260, 'genre': 'Fantasy', 'age_restriction': 'G'}
     ]
 
     # TODO: แสดงเมนูให้ผู้ใช้เลือก
