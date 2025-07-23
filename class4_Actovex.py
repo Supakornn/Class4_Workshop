a1c16257fcf2621a6ea84fc82e5f36248b5c492a
Supakornn <78318848+Supakornn@users.noreply.github.com>
2025-07-23 23:51:06 +0700

    edit age

--- class4_Actovex.py
+++ class4_Actovex.py
@@ -27,10 +27,10 @@ def buy_ticket(movie_list):
 def main():
     # TODO: สร้างรายการหนังเป็น list ของ dict โดยเก็บข้อมูล movie_name, ticket_price, genre, age_restriction
     movies = [
-        {'movie_name': 'Avengers Endgame', 'ticket_price': 300, 'genre': 'Action', 'age_restriction': '13+'},
-        {'movie_name': 'Inception', 'ticket_price': 280, 'genre': 'Sci-Fi', 'age_restriction': '13+'},
-        {'movie_name': 'It', 'ticket_price': 180, 'genre': 'Horror', 'age_restriction': '18+'},
-        {'movie_name': 'The Notebook', 'ticket_price': 250, 'genre': 'Romantic', 'age_restriction': '13+'},
+        {'movie_name': 'Avengers Endgame', 'ticket_price': 300, 'genre': 'Action', 'age_restriction': '13'},
+        {'movie_name': 'Inception', 'ticket_price': 280, 'genre': 'Sci-Fi', 'age_restriction': '13'},
+        {'movie_name': 'It', 'ticket_price': 180, 'genre': 'Horror', 'age_restriction': '18'},
+        {'movie_name': 'The Notebook', 'ticket_price': 250, 'genre': 'Romantic', 'age_restriction': '13'},
         {'movie_name': 'Harry Potter and the Sorcerer\'s Stone', 'ticket_price': 260, 'genre': 'Fantasy', 'age_restriction': 'G'}
     ]
 
